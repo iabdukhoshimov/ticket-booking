cf9d258b1d38f40aef7b11db8d1edd39dd9c9483
an-ikhtiyour <ikhtiyarabdukhashimov@Ikhtiyars-MacBook-Air.local>
2022-05-06 09:38:51 +0500

    added: feedback to user

--- main.go
+++ main.go
@@ -64,8 +64,15 @@ func main() {
 				break
 			}
 		} else {
-			fmt.Println("Your input date is invalid, Please try again")
-			continue
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
 		}
 
 	}

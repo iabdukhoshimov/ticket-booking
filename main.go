e2cb57239422a66dfc71e0635740d000de7b4304
an-ikhtiyour <ikhtiyarabdukhashimov@Ikhtiyars-MacBook-Air.local>
2022-05-10 12:23:48 +0500

    functions added

--- main.go
+++ main.go
@@ -14,48 +14,15 @@ func main() {
 	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	for {
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-
-		fmt.Println("Please enter your name:")
-		fmt.Scan(&firstName)
-
-		fmt.Println("Please enter your last name:")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Please enter your email:")
-		fmt.Scan(&email)
-
-		fmt.Println("Please enter how many tickets you want to book?")
-		fmt.Scan(&userTickets)
-
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 		if isValidName && isValidEmail && isValidTicketNumber {
 
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName)
+			bookTicket(remainingTickets, userTickets, bookings, firstName, lastName, email, conferenceName)
 
-			// fmt.Printf("The whole array %v\n", bookings)
-			// fmt.Printf("The first element array %v\n", bookings[0])
-			// fmt.Printf("The type of the array %T\n", bookings)
-			// fmt.Printf("The size of the array %v\n", len(bookings))
-
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will be recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
-
-			fmt.Printf("%v this many tickets available for this %v \n", remainingTickets, conferenceName)
-
-			firstNames := []string{}
-
-			for _, booking := range bookings {
-				var names = strings.Fields(booking)
-				firstNames = append(firstNames, names[0])
-			}
-
+			firstNames := getFirstNames(bookings)
 			fmt.Printf("The first names of bookings are %v\n", firstNames)
+
 			if remainingTickets == 0 {
 				fmt.Println("Our conference is booked out. Come back next year")
 				break
@@ -79,4 +46,51 @@ func greetUsers(confName string, conferenceTickets int, remainingTickets uint) {
 	fmt.Printf("Welcome to %v booking application\n", confName)
 	fmt.Printf("We have total %v tickets and available %v \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames(bookings []string) []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Please enter your name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("Please enter how many tickets you want to book?")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(remainingTickets uint, userTickets uint, bookings []string, firstName string, lastName string, email string, conferenceName string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will be recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v this many tickets available for this %v \n", remainingTickets, conferenceName)
 }

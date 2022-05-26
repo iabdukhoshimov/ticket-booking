19f08b039109a3baf7521286f7a942f441690633
an-ikhtiyour <ikhtiyarabdukhashimov@Ikhtiyars-MacBook-Air.local>
2022-05-26 08:35:40 +0500

    go routine added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
+	"time"
 )
 
 const conferenceTickets = 50
@@ -30,6 +31,7 @@ func main() {
 		if isValidName && isValidEmail && isValidTicketNumber {
 
 			bookTicket(userTickets, firstName, lastName, email)
+			sendTicket(userTickets, firstName, lastName, email)
 
 			firstNames := getFirstNames()
 			fmt.Printf("The first names of bookings are %v\n", firstNames)
@@ -105,4 +107,12 @@ func bookTicket(userTickets uint, firstName string, lastName string, email strin
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will be recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 	fmt.Printf("%v this many tickets available for this %v \n", remainingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v ", userTickets, firstName, lastName)
+	fmt.Println("########################")
+	fmt.Printf("Sending ticket:\n %v\nto email address %v\n", ticket, email)
+	fmt.Println("########################")
 }
